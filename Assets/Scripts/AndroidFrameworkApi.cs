4b37b644b12964c94d08dbf066a9bd9b535bf36f
hawk_wei <hawk_wei@htc.com>
2021-07-14 15:55:45 +0800

    Call Android APIs from Unity to AndroidFramework

--- Assets/Scripts/AndroidFrameworkApi.cs
+++ Assets/Scripts/AndroidFrameworkApi.cs
@@ -1,11 +1,124 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿#if UNITY_ANDROID && !UNITY_EDITOR
+// #if UNITY_ANDROID 
+#define ANDROID_DEVICE
+#endif
+
+using System;
+using Htc.Viveport;
 using UnityEngine;
 
 public class AndroidFrameworkApi : MonoBehaviour
 {
-    static public void triggerVibration() {
+    private AndroidJavaObject mUnityActivity;
+    private AndroidJavaObject mUnityApplicationContext;
+
+    private AndroidJavaObject mAndroidApisObj;
+    private AndroidJavaClass  mAndroidApisClz;
+
+    private void Start()
+    {
+        mUnityActivity = UnityActivityHelper.GetActivity();
+        mUnityApplicationContext = UnityActivityHelper.GetApplicationContext(mUnityActivity);
+#if ANDROID_DEVICE
+        if (mUnityActivity == null)
+            Debug.LogError("AndroidFrameworkApi  Start()    mUnityActivity == null");
+
+        // mAndroidApisClz = new AndroidJavaClass("com.htc.svr.androidSettingsLibrary.AndroidApis");
+        mAndroidApisObj = new AndroidJavaObject("com.htc.svr.androidSettingsLibrary.AndroidApis");
+        mAndroidApisObj.Call("setContext", mUnityActivity);
+#else
+        Debug.LogError("Not support this Script on non-Android device");
+#endif
+    }
+
+    public void triggerVibration() {
         Debug.Log("triggerVibration !!!");
         Handheld.Vibrate();
+    }
+
+    public void adjustPlayArea(int areaSize) // normal key + priv-app
+    {
+#if ANDROID_DEVICE
+        try
+        {
+            mAndroidApisObj.Call("adjustPlayArea", areaSize);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("AndroidFrameworkApi   adjustPlayArea     failed!");
+        }
+#else
+        Debug.LogError("Not support this API on non-Android device");
+#endif
+    }
+
+    public void enableDoubleTapPassThrough(bool enable) // normal key + priv-app + Fix AIDL duplicate issue
+    {
+#if ANDROID_DEVICE
+        try
+        {
+            mAndroidApisObj.Call("enableDoubleTapPassThrough", enable);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("AndroidFrameworkApi   enableDoubleTapPassThrough    failed!");
+        }
+#else
+        Debug.LogError("Not support this API on non-Android device");
+#endif
+    }
+
+    public void enableCarMode(bool enable) // normal key + priv-app
+    {
+#if ANDROID_DEVICE
+        try
+        {
+            mAndroidApisObj.Call("enableCarMode", enable);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("AndroidFrameworkApi   enableCarMode    failed!");
+        }
+#else
+        Debug.LogError("Not support this API on non-Android device");
+#endif
+    }
+
+    public void enableVirtualWall(bool enable) // normal key
+    {
+#if ANDROID_DEVICE
+        try
+        {
+            mAndroidApisObj.Call("enableVirtualWall", enable);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("AndroidFrameworkApi   enableVirtualWall    failed!");
+        }
+#else
+        Debug.LogError("Not support this API on non-Android device");
+#endif
+    }
+
+    public void enterPhoneMode(bool enable) // normal key
+    {
+#if ANDROID_DEVICE
+        try
+        {
+            mAndroidApisObj.Call("enterPhoneMode", enable);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("AndroidFrameworkApi   enterPhoneMode    failed!");
+        }
+#else
+        Debug.LogError("Not support this API on non-Android device");
+#endif
+    }
+
+    void DebugSrackInfo()
+    {
+        string trackStr = new System.Diagnostics.StackTrace().ToString();
+        Debug.Log("Stack Info:" + trackStr);
     }
 }
---
+++ Assets/Scripts/UnityActivityHelper.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Htc.Viveport
+{
+    public static class UnityActivityHelper
+    {
+        private const string UNITY_PLAYER_CLASS = "com.unity3d.player.UnityPlayer";
+
+        //#if UNITY_ANDROID && !UNITY_EDITOR
+        /// Returns the Android Activity used by the Unity device player. The caller is
+        /// responsible for memory-managing the returned AndroidJavaObject.
+        public static AndroidJavaObject GetActivity()
+        {
+            AndroidJavaClass jc = new AndroidJavaClass(UNITY_PLAYER_CLASS);
+            if (jc == null)
+            {
+                Debug.LogErrorFormat("Failed to get class {0}", UNITY_PLAYER_CLASS);
+                return null;
+            }
+            AndroidJavaObject activity = jc.GetStatic<AndroidJavaObject>("currentActivity");
+            if (activity == null)
+            {
+                Debug.LogError("Failed to obtain current Android activity.");
+                return null;
+            }
+            return activity;
+        }
+
+        /// Returns the application context of the current Android Activity.
+        public static AndroidJavaObject GetApplicationContext(AndroidJavaObject activity)
+        {
+            AndroidJavaObject context = activity.Call<AndroidJavaObject>("getApplicationContext");
+            if (context == null)
+            {
+                Debug.LogError("Failed to get application context from Activity.");
+                return null;
+            }
+            return context;
+        }
+    }
+}
